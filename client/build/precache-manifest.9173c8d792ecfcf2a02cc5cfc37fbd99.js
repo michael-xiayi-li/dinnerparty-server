82e198582540dabb71a16c71cb88277870aa2781
EC2 Default User <ec2-user@ip-172-31-24-20.ec2.internal>
2019-05-31 17:58:59 +0000

    localhostfix

---
+++ client/build/precache-manifest.9173c8d792ecfcf2a02cc5cfc37fbd99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46caf27aeb69cec82796",
+    "url": "/static/js/main.46caf27a.chunk.js"
+  },
+  {
+    "revision": "9f5dd5278c190d797fcf",
+    "url": "/static/js/1.9f5dd527.chunk.js"
+  },
+  {
+    "revision": "46caf27aeb69cec82796",
+    "url": "/static/css/main.8575fbd5.chunk.css"
+  },
+  {
+    "revision": "b41238472ac742060c1246e917a511fe",
+    "url": "/index.html"
+  }
+];
